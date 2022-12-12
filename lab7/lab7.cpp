c86c455e8e07a1c0cb767e47949bb21d82611fa6
maxkrivenya <maxkrivenya@gmail.com>
2022-12-12 20:59:32 +0300

    hi

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -5,14 +5,17 @@ int main()
     int a = 0;
     int* A = &a;
     int again = 0;
+
     do {
         a = var(a);
         rewind(stdin);
         p = char2fill(p, a);
         p = char2adjust(p, a);
+
         p = strtoword(p, a, A, a - 1);
         strsort(p, *A);
         finishup(p, *A);
+
         again = exitf(again);
     } while (!again);
     return 0;
